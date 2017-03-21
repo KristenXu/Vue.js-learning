cd3390458093dc5e31ff7872acf292334ed09df8
Chainn <578579786@qq.com>
2017-03-21 13:44:48 +0800

    Add start

---
+++ version1.0/start/app.js
@@ -0,0 +1,25 @@
+new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue.js!',
+        newTodo: '',
+        todos: [
+            { text: 'Learn JavaScript' },
+            { text: 'Learn Vue.js' },
+            { text: 'Build Something Awesome' }
+        ]
+    },
+    methods: {
+        reverseMesssage: function () {
+            this.message = this.message.split('').reverse().join('')
+        },
+        addTodo: function () {
+            this.todos.push({
+                text: this.newTodo
+            })
+        },
+        removeTodo: function (index) {
+            this.todos.splice(index, 1)
+        }
+    }
+});
