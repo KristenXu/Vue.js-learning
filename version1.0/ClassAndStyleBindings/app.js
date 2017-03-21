f862397b04984c27b26faeb903c81cbe03b50ba2
Chainn <578579786@qq.com>
2017-03-21 17:00:03 +0800

    Add class and style bindings

---
+++ version1.0/ClassAndStyleBindings/app.js
@@ -0,0 +1,24 @@
+var vm = new Vue({
+    el: '#example',
+    data: {
+        isA: true,
+        isB: false,
+        classObject: {
+            isA: true,
+            isB: false,
+        },
+        classA: 'class-a',
+        classB: 'class-b',
+        styleObject: {
+            color: 'red',
+            fontSize: '13px'
+        },
+        styleObjectA: {
+            color: '#3899ec',
+            fontSize: '13px'
+        },
+        styleObjectB: {
+            opacity: 0.6
+        }
+    }
+})
