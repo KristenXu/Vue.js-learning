759f14dc044fb932eca45de2fb2d9fba5ad4eb9f
Chainn <578579786@qq.com>
2017-03-22 10:46:25 +0800

    Add event handing

---
+++ version1.0/EventHandling/app.js
@@ -0,0 +1,15 @@
+new Vue({
+    el: '#example',
+    methods: {
+        sayMsg: function (msg) {
+            alert(msg)
+        },
+        clickEvent: function (event) {
+            // alert(event.target.tagName)
+            console.log('event', event)
+        },
+        click$event: function ($event) {
+            console.log('$event', $event)
+        }
+    }
+})
